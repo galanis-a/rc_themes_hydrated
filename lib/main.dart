c58f1bba73d31e768d8d1aae13557f027955ffd7
Antonis Galanis <ant.galanis@gladd.gr>
2019-08-10 10:15:14 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'ui/global/theme/bloc/bloc.dart';
+import 'ui/home/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      builder: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+    return MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+      theme: state.themeData,
+    );
+  }
+}
